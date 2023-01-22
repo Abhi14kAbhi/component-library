3913b825285f61ec9592b967aa395bc63d8dd0c7
Abhishek Shah <abhisheks@geekyants.com>
2023-01-22 22:59:05 +0530

    Optimized builds using terser and added storybook initial setup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import typescript from "@rollup/plugin-typescript"; // typescript plugin
+import dts from "rollup-plugin-dts"; // Used to get .d.ts files in node modules when the package is used to get types
+import terser from "@rollup/plugin-terser"; // Used for optimization and smaller builds
+import peerDepsExternal from "rollup-plugin-peer-deps-external"; // dont know, for peer dependencies, not exactly sure
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -11,19 +13,21 @@ export default [
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
+        format: "cjs", // common js bundle format
+        sourcemap: true, // source map is used to debug, if there is any errors/warnings
       },
       {
         file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
+        format: "esm", // es module bundle format
+        sourcemap: true, // source map is used to debug, if there is any errors/warnings
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
