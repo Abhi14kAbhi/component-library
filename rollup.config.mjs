600013c921fd5c7eb30b6be5d0823be2fc45c25a
Abhishek Shah <abhisheks@geekyants.com>
2023-01-26 12:39:50 +0530

    storybook configured

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,6 +10,7 @@ import packageJson from "./package.json" assert { type: "json" };
 export default [
   {
     input: "src/index.ts",
+    external: ["react-dom"],
     output: [
       {
         file: packageJson.main,
