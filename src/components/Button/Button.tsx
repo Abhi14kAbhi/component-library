78bf966c12d04fe067b87bc9068d3108615e74c1
Abhishek Shah <abhisheks@geekyants.com>
2023-02-12 00:31:38 +0530

    Default Colors and spacing added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 interface ButtonProps {
   label: string;
+  size: string;
+  disabled: string;
+  // onClick: () => {};
+  variant: string;
+  children: ReactNode | string;
+  leftAdornmentIcon: ReactNode;
+  rightAdormentIcon: ReactNode;
+  edge: string;
+  style: Object;
 }
 
 const Button = (props: ButtonProps) => {
+  const {
+    label,
+    size,
+    children,
+    disabled,
+    edge,
+    leftAdornmentIcon,
+    rightAdormentIcon,
+    style,
+    variant,
+  } = props;
+
   return (
-    <div className="p-11">
-      <button>{props.label}</button>
+    <div className="flex" onClick={() => console.log("herere")}>
+      {leftAdornmentIcon ? <div>{leftAdornmentIcon}</div> : <div />}
+      <div>{children || label}</div>
+      {rightAdormentIcon ? <div>{rightAdormentIcon}</div> : <div />}
     </div>
   );
 };
